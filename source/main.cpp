e6945433ef762452df498f7ebc7abfd56c3aadb0
Reklov42 <main@mail.reklov.su>
2025-11-29 00:53:41 +0300

    Some entry point rework

--- source/main.cpp
+++ source/main.cpp
@@ -7,16 +7,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 
 #include <vector>
 #include <map>
 #include <thread>
 
 #include "deltamake.h"
+#include "Terminal.h"
+
 #include "Exception.h"
-#include "Solutions.h"
-#include "WorkerPool.h"
+#include "SolutionDefault.h"
+#include "Workers.h"
 
 using namespace DeltaMake;
 
@@ -24,8 +25,6 @@ using namespace DeltaMake;
 
 DeltaMake::SConfig g_config;
 extern DeltaMake::SConfig* const DeltaMake::config = &g_config;
-
-CWorkerPool g_pool;
 
 // ******************************************************************************** //
 										//										//
@@ -49,39 +48,6 @@ class CArgStream {
 		const char*						m_arg;
 };
 
-/**
- * 
- */
-class CTerminalLocal final : public DeltaMake::ITerminal {
-	public:
-		virtual							~CTerminalLocal() override				= default;
-
-		void							Init();
-
-		virtual void					MoveUp(size_t offset) override;
-		virtual void					MoveDown(size_t offset) override;
-		virtual void					MoveRight(size_t offset) override;
-		virtual void					MoveLeft(size_t offset) override;
-
-		virtual void					Flush() override;
-		virtual void					ShowCursor(bool bShow) override;
-
-		virtual int						Log(ELogLevel level, const char format[], ...) override;
-
-		virtual size_t					GetColumns() const override;
-		virtual size_t					GetRows() const override;
-
-		virtual int						ExecSystem(const char cmd[]) override;
-		virtual time_t					GetLastModificationTime(const char path[]) override;
-
-	private:
-		size_t							m_nColumns;
-		size_t							m_nRows;
-};
-
-CTerminalLocal g_terminalLocal;
-extern DeltaMake::ITerminal* const DeltaMake::terminal = &g_terminalLocal;
-
 // ******************************************************************************** //
 
 /**
@@ -90,8 +56,8 @@ extern DeltaMake::ITerminal* const DeltaMake::terminal = &g_terminalLocal;
 void ParseArgs(CArgStream&& stream);
 
 /**
- * If `arg[0] == '-'` then `-name[0]`
- * if `arg[0:1] == '--'` then `--name`
+ * If `arg[0] == '-'` then `'-'name[0]`
+ * if `arg[0:1] == '--'` then `'--'name`
  */
 bool CheckArg(const char arg[], const char name[]);
 
@@ -106,9 +72,9 @@ void PrintHelp();
 void Init();
 
 /**
- * SIGINT Handler
+ * Register plugin factory
  */
-void IntHandler(int signal);
+void RegisterPlugin(IPlugin* plugin);
 
 
 #include "AutoGen.h"
@@ -117,7 +83,7 @@ void IntHandler(int signal);
  * Entry point
  */
 int main(int argc, char* argv[]) {
-	g_terminalLocal.Init();
+	terminal->Init();
 
 	terminal->Log(
 		LOG_INFO,
@@ -130,6 +96,14 @@ int main(int argc, char* argv[]) {
 
 	//
 	Init();
+	LoadPlugins();
+
+
+
+	DeltaMake::scheduler->Init(g_config.nMaxWorkers);
+	//DeltaMake::scheduler->Start();
+
+	//return 0;
 
 	// Loading
 	try {
@@ -180,28 +154,20 @@ int main(int argc, char* argv[]) {
 		terminal->Log(LOG_DETAIL, "\t\"%s\"\n", g_config.builds[i]);
 	}
 
+	ITaskList* taskList = scheduler->GetList();
+
 	for (size_t i = 0; i < g_config.builds.size(); ++i) {
 		builders[i]->PreBuild();
-		builders[i]->Build(&g_pool);
+		builders[i]->Build(taskList);
 	}
 
-	if (g_pool.Size() == 0) {
+	if (taskList->GetTaskCount() == 0) {
 		terminal->Log(LOG_INFO, "Nothing to do.\n");
 		return EXIT_SUCCESS;
 	}
 
 	// DANGER: THREADS!
-	{
-		struct sigaction intHandler;
-
-		intHandler.sa_handler = IntHandler;
-		sigemptyset(&intHandler.sa_mask);
-		intHandler.sa_flags = 0;
-
-		sigaction(SIGINT, &intHandler, &g_config.oldHandler);
-
-		g_pool.Start(g_config.nMaxWorkers);
-	}
+	DeltaMake::scheduler->Start();
 
 	for (size_t i = 0; i < g_config.builds.size(); ++i)
 		builders[i]->PostBuild();
@@ -353,142 +319,19 @@ void Init() {
 /* ****************************************
  * Init
  */
-void IntHandler(int signal) {
-	sigaction(SIGINT, &g_config.oldHandler, NULL);
+void RegisterPlugin(IPlugin* plugin) {
+	const EPluginType type = plugin->GetType();
 
-	terminal->ShowCursor(true);
-	terminal->Log(LOG_ERROR, "\n\n\n\n\nCaught signal: %i\nStopping workers... ", signal);
-
-	g_pool.Stop();
-	terminal->Log(LOG_ERROR, "\nDone. Exit\n");
-
-	exit(EXIT_FAILURE);
-}
-
-// ******************************************************************************** //
-
-#if defined(__linux__)
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-
-/* ****************************************
- * CTerminalLocal::Init
- */
-void CTerminalLocal::Init() {
-	struct winsize ws;
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-
-	m_nColumns = static_cast<size_t>(ws.ws_col);
-	m_nRows = static_cast<size_t>(ws.ws_row);
-}
-
-/* ****************************************
- * CTerminalLocal::MoveUp
- */
-void CTerminalLocal::MoveUp(size_t offset) {
-	printf("\033[%zuA", offset);
-}
-
-/* ****************************************
- * CTerminalLocal::MoveDown
- */
-void CTerminalLocal::MoveDown(size_t offset) {
-	printf("\033[%zuB", offset);
-}
-
-/* ****************************************
- * CTerminalLocal::MoveRight
- */
-void CTerminalLocal::MoveRight(size_t offset) {
-	printf("\033[%zuC", offset);
-}
-
-/* ****************************************
- * CTerminalLocal::MoveLeft
- */
-void CTerminalLocal::MoveLeft(size_t offset) {
-	printf("\033[%zuD", offset);
-}
-
-/* ****************************************
- * CTerminalLocal::Flush
- */
-void CTerminalLocal::Flush() {
-	fflush(stdout);
-}
-
-/* ****************************************
- * CTerminalLocal::ShowCursor
- */
-void CTerminalLocal::ShowCursor(bool bShow) {
-	printf((bShow == true) ? "\033[?25h" : "\033[?25l");
-}
-
-/* ****************************************
- * CTerminalLocal::Log
- */
-int CTerminalLocal::Log(ELogLevel level, const char format[], ...) {
-	if ((level == ELogLevel::LOG_DETAIL) && (g_config.bVerbose == false))
-		return 0;
-
-	FILE* const out = (level == ELogLevel::LOG_ERROR) ? stderr : stdout;
-
-	switch (level) {
-		case ELogLevel::LOG_ERROR:		fputs("\033[0;31m", out); break; // Red
-		case ELogLevel::LOG_WARNING:	fputs("\033[0;33m", out); break; // Yellow
-		case ELogLevel::LOG_DETAIL:		fputs("\033[0;36m", out); break; // Cyan
-	
-		default:						fputs("\033[0m", out); break; // Reset
+	switch (type) {
+		case EPluginType::SOLUTION: {
+				ISolutionFactory* factory = static_cast<ISolutionFactory*>(plugin);
+				g_config.solutionTypes[factory->GetName()] = factory;
+				terminal->Log(LOG_DETAIL, "Solution plugin loaded: %s\n", factory->GetName().c_str());
+			}
+			break;
+		
+		default:
+			terminal->Log(LOG_WARNING, "Unknown plugin type (%i). Ignoring...\n", static_cast<int>(type));
+			break;
 	}
-
-	va_list argptr;
-	va_start(argptr, format);
-	int size = vfprintf(out, format, argptr);
-	va_end(argptr);
-
-	fputs("\033[0m", out); // Reset color
-
-	return size;
-}
-
-
-/* ****************************************
- * CTerminalLocal::ExecSystem
- */
-int CTerminalLocal::ExecSystem(const char cmd[]) {
-	int status = system(cmd);
-    status = WIFEXITED(status) ? WEXITSTATUS(status) : 0;
-	
-	// TODO: Save exit
-	if (status != 0)
-		exit(EXIT_FAILURE);
-
-	return status;
-}
-
-/* ****************************************
- * CTerminalLocal::GetLastModificationTime
- */
-time_t CTerminalLocal::GetLastModificationTime(const char path[]) {
-	struct stat stats;
-	stat(path, &stats);
-	
-	return stats.st_mtime;
-}
-
-#endif
-
-/* ****************************************
- * CTerminalLocal::GetColumns
- */
-size_t CTerminalLocal::GetColumns() const {
-	return m_nColumns;
-}
-
-/* ****************************************
- * CTerminalLocal::GetRows
- */
-size_t CTerminalLocal::GetRows() const {
-	return m_nRows;
 }
